35dc8be255be6d81b4a8b2b91d559e3c8e13646c
Gowtham T G <gowthamgopinath1@gmail.com>
2022-08-25 11:33:29 +0530

    API changed

--- src/app/services/api-client.service.ts
+++ src/app/services/api-client.service.ts
@@ -1,18 +1,18 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class ApiClientService {
-  host: string = 'http://localhost:5000';
+  host: string = "https://git.heroku.com/library-backend-devrev.git";
 
   constructor(private http: HttpClient) {}
 
   throwError(err: { error: { errorMessage: string } }): string {
     let message: string = err.error.errorMessage;
-    throw message || 'Please try later';
+    throw message || "Please try later";
   }
 
   getBooksCondition(filterDetails: any): Observable<any> {
